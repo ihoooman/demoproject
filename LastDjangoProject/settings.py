45f9577242f41613cce58f4fa44edb783ca14ba8
Hooman Maddah <hoomi.hooman@gmail.com>
2025-05-24 10:37:35 +0330

    Load environment variables from .env file and update database settings to use dj-database-url

--- LastDjangoProject/settings.py
+++ LastDjangoProject/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -112,7 +115,7 @@ DATABASES = {
         default=os.environ['DATABASE_URL'],
         conn_max_age=600,
         #ssl_require=True
-        ssl_require=not bool(os.getenv("CI", False)),
+        #ssl_require=not bool(os.getenv("CI", False)),
     )
 }
 
