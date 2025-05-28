ddc3c30d4e5f3ac2353755437a0d36d92c8fadef
Hooman Maddah <82512236+ihoooman@users.noreply.github.com>
2025-05-28 10:48:02 +0330

    Remove commented-out code from models, urls, and views files

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,32 +1,4 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from . import views
-# from .views import GroupListView, GroupCreateView, delete_group, delete_multiple_groups
-#
-# app_name = 'dashboard'
-#
-# urlpatterns = [
-#     # Group management URLs
-#     path('', GroupListView.as_view(), name='group_list'),
-#     path('group/add/', GroupCreateView.as_view(), name='group_add'),
-#     path('group/<int:parent_pk>/children/', GroupListView.as_view(), name='group_children'),
-#     path('group/<int:parent_pk>/add/', GroupCreateView.as_view(), name='group_add_child'),
-#     path('group/<int:pk>/form/', views.SubgroupFormView.as_view(), name='subgroup_form'),
-#     path('group/<int:group_id>/save-responses/', views.save_subgroup_responses, name='save_subgroup_responses'),
-#
-#     # Delete functionality
-#     path('group/<int:pk>/delete/', delete_group, name='group_delete'),
-#     path('groups/delete/', delete_multiple_groups, name='groups_delete'),
-#
-#     # Category management URLs
-#     path('categories/', views.CategoryListView.as_view(), name='categories'),
-#     path('categories/<int:pk>/', views.SubcategoryListView.as_view(), name='subcategories'),
-#     path('categories/<int:category_id>/save/', views.save_responses, name='save_responses'),
-#
-#     # Administrative URLs
-#     path('admin/', admin.site.urls),
-#     path('accounts/', include('accounts.urls')),
-# ]
+
 from django.contrib import admin
 from django.urls import path, include
 from . import views
